b100a3e25be30b4b6f88b08e8088a6e3491acc16
Alex Thiessen <alex.thiessen.de+github@gmail.com>
2018-12-23 18:21:29 +0100

    aliases/git-brief: Replace upstream aliases
    Replace upstream aliases with {k,m,}git{a,d,dc,s,} where the prefixes
    denote directories to change to and the suffixes denote operations to
    perform.

--- aliases/available/git-brief.aliases.bash
+++ aliases/available/git-brief.aliases.bash
@@ -1,88 +1,35 @@
 cite 'about-alias'
-about-alias 'common git abbreviations'
+about-alias 'brief git abbreviations'
 
-# Aliases
-alias gcl='git clone'
-alias ga='git add'
-alias grm='git rm'
-alias gap='git add -p'
-alias gall='git add -A'
-alias gf='git fetch --all --prune'
-alias gft='git fetch --all --prune --tags'
-alias gfv='git fetch --all --prune --verbose'
-alias gftv='git fetch --all --prune --tags --verbose'
-alias gus='git reset HEAD'
-alias gpristine='git reset --hard && git clean -dfx'
-alias gclean='git clean -fd'
-alias gm="git merge"
-alias gmv='git mv'
-alias g='git'
-alias get='git'
-alias gst='git status'
-alias gs='git status'
-alias gss='git status -s'
-alias gsu='git submodule update --init --recursive'
-alias gl='git pull'
-alias glum='git pull upstream master'
-alias gpr='git pull --rebase'
-alias gpp='git pull && git push'
-alias gup='git fetch && git rebase'
-alias gp='git push'
-alias gpo='git push origin'
-alias gpu='git push --set-upstream'
-alias gpuo='git push --set-upstream origin'
-alias gpom='git push origin master'
-alias gr='git remote'
-alias grv='git remote -v'
-alias gra='git remote add'
-alias gd='git diff'
-alias gdv='git diff -w "$@" | vim -R -'
-alias gc='git commit -v'
-alias gca='git commit -v -a'
-alias gcm='git commit -v -m'
-alias gcam="git commit -v -am"
-alias gci='git commit --interactive'
-alias gb='git branch'
-alias gba='git branch -a'
-alias gbt='git branch --track'
-alias gbm='git branch -m'
-alias gbd='git branch -d'
-alias gbD='git branch -D'
-alias gcount='git shortlog -sn'
-alias gcp='git cherry-pick'
-alias gco='git checkout'
-alias gcom='git checkout master'
-alias gcb='git checkout -b'
-alias gcob='git checkout -b'
-alias gct='git checkout --track'
-alias gexport='git archive --format zip --output'
-alias gdel='git branch -D'
-alias gmu='git fetch origin -v; git fetch upstream -v; git merge upstream/master'
-alias gll='git log --graph --pretty=oneline --abbrev-commit'
-alias gg="git log --graph --pretty=format:'%C(bold)%h%Creset%C(magenta)%d%Creset %s %C(yellow)<%an> %C(cyan)(%cr)%Creset' --abbrev-commit --date=relative"
-alias ggs="gg --stat"
-alias gsl="git shortlog -sn"
-alias gwc="git whatchanged"
-alias gt="git tag"
-alias gta="git tag -a"
-alias gtd="git tag -d"
-alias gtl="git tag -l"
-# From http://blogs.atlassian.com/2014/10/advanced-git-aliases/
-# Show commits since last pull
-alias gnew="git log HEAD@{1}..HEAD@{0}"
-# Add uncommitted and unstaged changes to the last commit
-alias gcaa="git commit -a --amend -C HEAD"
-alias ggui="git gui"
-alias gcsam="git commit -S -am"
-alias gstd="git stash drop"
-alias gstl="git stash list"
-alias gh='cd "$(git rev-parse --show-toplevel)"'
+# Check
+command -v git &>/dev/null || return
 
-case $OSTYPE in
-  darwin*)
-    alias gtls="git tag -l | gsort -V"
-    ;;
-  *)
-    alias gtls='git tag -l | sort -V'
-    ;;
-esac
+# Subdirectories as prefixes
+declare -A git_prefixes=(
+  [m]=main
+  [k]=kissy_build
+)
+
+# Common operations as suffixes
+declare -A git_suffixes=(
+  [a]="add --interactive"
+  [d]=diff
+  [dc]="diff --cached"
+  [s]=status
+)
+
+for git_prefix_key in "${!git_prefixes[@]}" ""
+do
+  for git_suffix_key in "${!git_suffixes[@]}" ""
+  do
+    ALIAS="${git_prefix_key}git${git_suffix_key}"
+    [ "${ALIAS}" != "git" ] || continue
+    DIR_CLAUSE="${git_prefix_key:+-C \"${git_prefixes[${git_prefix_key}]}\"}"
+    OP_CLAUSE="${git_suffix_key:+${git_suffixes[${git_suffix_key}]}}"
+
+    # shellcheck disable=SC2139 disable=SC2140
+    alias "${ALIAS}"="git ${DIR_CLAUSE} ${OP_CLAUSE}"
+
+    unset ALIAS DIR_CLAUSE OP_CLAUSE
+  done
+done
