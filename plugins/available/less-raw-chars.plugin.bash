71cc36bdcc3e3a89c312c1f11923e909e478e296
Alex Thiessen <alex.thiessen.de+github@gmail.com>
2018-12-23 22:26:49 +0100

    plugins: Add `less-raw-chars`
    Make `less` interpret color escape sequences.

---
+++ plugins/available/less-raw-chars.plugin.bash
@@ -0,0 +1,10 @@
+# make `less` interpret color escape sequences
+
+cite about-plugin
+# shellcheck disable=2016
+about-plugin 'make `less` interpret color escape sequences'
+
+# Check
+command -v less &>/dev/null || return
+
+export LESS="${LESS:+${LESS} }--RAW-CONTROL-CHARS"
