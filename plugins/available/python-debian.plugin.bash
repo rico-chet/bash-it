4fff96a95103071cdbb7224e34ded3af9e35b7ad
Alex Thiessen <alex.thiessen.de+github@gmail.com>
2018-12-24 16:46:17 +0100

    plugins: Add `python-debian`
    Add a plugin which enables site-packages on debian systems.

---
+++ plugins/available/python-debian.plugin.bash
@@ -0,0 +1,15 @@
+cite about-plugin
+about-plugin 'add python site-packages for separate storage of user-built modules'
+
+# For now, python 2.7 only
+
+# Check
+command -v python2 || return "${SKIPPED}"
+{ python2 --version | grep --quiet '^Python 2\.7' ;} || return
+[ -r /etc/os-release ] \
+	&& grep --quiet debian /etc/os-release \
+	&& [ -d "/usr/local/lib/python2.7/site-packages" ] \
+	|| return "${SKIPPED}"
+
+# https://wiki.debian.org/Python#Deviations_from_upstream
+export PYTHONPATH="/usr/local/lib/python2.7/site-packages${PYTHONPATH:+:$PYTHONPATH}"
