c44ad8cabcf6bb7eb82acf4a2e7e3bd2ff503663
Alex Thiessen <alex.thiessen.de+github@gmail.com>
2019-01-18 21:11:19 +0100

    install: Enable `proxy-func` plugin per default
    I use proxies at work, so enable the helper function per default.

--- install.sh
+++ install.sh
@@ -175,6 +175,7 @@ else
   _enable-plugin history-custom
   _enable-plugin keychain
   _enable-plugin less-raw-chars
+  _enable-plugin proxy-func
   _enable-plugin stderred
   _enable-plugin stow
   _enable-alias fuck
