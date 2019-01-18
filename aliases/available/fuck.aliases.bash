e3217fdd53dc1e050cf000fb164525cbcb2eae4e
Alex Thiessen <alex.thiessen.de+github@gmail.com>
2019-01-18 13:19:35 +0100

    aliases/fuck: Add checks
    Check that `thefuck` is installed and that any upcoming calls would
    succeed.

--- aliases/available/fuck.aliases.bash
+++ aliases/available/fuck.aliases.bash
@@ -1,3 +1,9 @@
 # https://github.com/nvbn/thefuck
+
+# Check
+command -v thefuck 1>/dev/null || return "${SKIPPED}"
+fc -ln -1 &>/dev/null || return 1
+thefuck $(fc -ln -1) &>/dev/null || return 2
+
 alias fuck='eval $(thefuck $(fc -ln -1))'
 alias please='fuck'
