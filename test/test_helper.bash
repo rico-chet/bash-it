abc29f1a45a8cd3629cd3686487cca2403a7e9c6
Alex Thiessen <alex.thiessen.de+github@gmail.com>
2020-04-10 00:57:09 +0000

    test/helper: Isolate tests further

--- test/test_helper.bash
+++ test/test_helper.bash
@@ -6,33 +6,13 @@ unset TODO
 unset SCM_CHECK
 unset BASH_IT_AUTOMATIC_RELOAD_AFTER_CONFIG_CHANGE
 
-BASH_IT_TEST_DIR="${BATS_TMPDIR}/.bash_it"
 
-# guard against executing this block twice due to bats internals
-if [ "$BASH_IT_ROOT" != "${BASH_IT_TEST_DIR}/root" ]; then
-  export BASH_IT_ROOT="${BASH_IT_TEST_DIR}/root"
-  export BASH_IT=$BASH_IT_TEST_DIR
-fi
 
 export TEST_MAIN_DIR="${BATS_TEST_DIRNAME}/.."
 export TEST_DEPS_DIR="${TEST_DEPS_DIR-${TEST_MAIN_DIR}/../test_lib}"
 
 # be independent of git's system configuration
 export GIT_CONFIG_NOSYSTEM
-
-# Some tools, e.g. `git` use configuration files from the $HOME directory,
-# which interferes with our tests. The only way to keep `git` from doing this
-# seems to set HOME explicitly to a separate location.
-# Refer to https://git-scm.com/docs/git-config#FILES.
-unset XDG_CONFIG_HOME
-export HOME="${BATS_TMPDIR}/home"
-mkdir -p "${HOME}"
-
-# For `git` tests to run well, user name and email need to be set.
-# Refer to https://git-scm.com/docs/git-commit#_commit_information.
-# This goes to the test-specific config, due to the $HOME overridden above.
-git config --global user.name "John Doe"
-git config --global user.email "johndoe@example.com"
 
 load "${TEST_DEPS_DIR}/bats-support/load.bash"
 load "${TEST_DEPS_DIR}/bats-assert/load.bash"
@@ -47,7 +27,28 @@ local_teardown() {
 }
 
 setup() {
+  export TEST_TEMP_DIR="$(temp_make --prefix 'bash-it-test-')"
+
+  export BASH_IT_TEST_DIR="${TEST_TEMP_DIR}/.bash_it"
+
+  export BASH_IT_ROOT="${BASH_IT_TEST_DIR}/root"
+  export BASH_IT=$BASH_IT_TEST_DIR
+
   mkdir -p -- "${BASH_IT_ROOT}"
+
+  # Some tools, e.g. `git` use configuration files from the $HOME directory,
+  # which interferes with our tests. The only way to keep `git` from doing this
+  # seems to set HOME explicitly to a separate location.
+  # Refer to https://git-scm.com/docs/git-config#FILES.
+  unset XDG_CONFIG_HOME
+  export HOME="${TEST_TEMP_DIR}"
+  mkdir -p -- "${HOME}"
+
+  # For `git` tests to run well, user name and email need to be set.
+  # Refer to https://git-scm.com/docs/git-commit#_commit_information.
+  # This goes to the test-specific config, due to the $HOME overridden above.
+  git config --global user.name "John Doe"
+  git config --global user.email "johndoe@example.com"
 
   local_setup
 }
@@ -56,6 +57,7 @@ teardown() {
   local_teardown
 
   rm -rf "${BASH_IT_TEST_DIR}"
+  temp_del "${TEST_TEMP_DIR}"
 }
 
 # Fail and display path of the link if it does not exist. Also fails
