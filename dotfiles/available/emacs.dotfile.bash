e5401d6d1a1341cc51a814d86f237cc3c3d290e5
Alex Thiessen <alex.thiessen.de+github@gmail.com>
2022-09-22 22:31:30 +0200

    dotfiles/emacs: Fix PWD -> BASH_IT

--- dotfiles/available/emacs.dotfile.bash
+++ dotfiles/available/emacs.dotfile.bash
@@ -8,8 +8,8 @@ command -v emacs 1>/dev/null || return "${SKIPPED}"
 
 # install
 cp --no-clobber --symbolic-link \
-  "${PWD}/dotfiles/available/.emacs.el" "${HOME}/.emacs.el"
+  "${BASH_IT}/dotfiles/available/.emacs.el" "${HOME}/.emacs.el"
 
 # check postcondition
 command -v diff 1>/dev/null && diff --brief \
-  "${PWD}/dotfiles/available/.emacs.el" "${HOME}/.emacs.el" >/dev/null
+  "${BASH_IT}/dotfiles/available/.emacs.el" "${HOME}/.emacs.el" >/dev/null
