45b12267363a053e06dc89f8159d2fc0962a16e3
Alex Thiessen <alex.thiessen.de+github@gmail.com>
2022-09-22 22:29:42 +0200

    dotfiles/editorconfig: Fix PWD -> BASH_IT

--- dotfiles/available/editorconfig.dotfile.bash
+++ dotfiles/available/editorconfig.dotfile.bash
@@ -8,8 +8,10 @@ command -v editorconfig 1>/dev/null || return "${SKIPPED}"
 
 # install
 cp --no-clobber --symbolic-link \
-  "${PWD}/dotfiles/available/.editorconfig" "${HOME}/.editorconfig"
+  "${BASH_IT}/dotfiles/available/.editorconfig" "${HOME}/.editorconfig"
 
 # check postcondition
-command -v diff 1>/dev/null && diff --brief \
-  "${PWD}/dotfiles/available/.editorconfig" "${HOME}/.editorconfig" >/dev/null
+command -v diff 1>/dev/null \
+&& diff --brief \
+  "${BASH_IT}/dotfiles/available/.editorconfig" \
+  "${HOME}/.editorconfig" >/dev/null
